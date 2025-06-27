3036a114cc18482ce5e916211c6f0b6e54c28462
SolomonPrince <jjons8622@gmail.com>
2025-06-27 16:35:30 +0500

    add webhook update method and service

---
+++ src/Entity/Responses/Webhook/WebhookEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace IikoApi\Entity\Responses\Webhook;
+
+use DateTimeImmutable;
+
+final readonly class WebhookEvent
+{
+    public function __construct(
+        public string    $eventType,
+        public DateTimeImmutable   $eventTime,
+        public string              $organizationId,
+        public string              $correlationId,
+        public DeliveryOrderUpdateEventInfo $eventInfo,
+    ) {}
+
+    public static function fromArray(array $d): self
+    {
+        return new self(
+            $d['eventType'],
+            new DateTimeImmutable($d['eventTime']),
+            $d['organizationId'],
+            $d['correlationId'],
+            DeliveryOrderUpdateEventInfo::fromArray($d['eventInfo'])
+        );
+    }
+}
